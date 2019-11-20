5fff98f58c4e623a4f0f35f1da4731ce66ff0c54
bairunxin <bairunxin@swifthealth.cn>
2019-11-20 20:10:12 +0800

    调整tabBar

--- fingertip/src/app/app.module.ts
+++ fingertip/src/app/app.module.ts
@@ -6,10 +6,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgZorroAntdMobileModule } from 'ng-zorro-antd-mobile';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TabBarComponent } from './shared/tab-bar/tab-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TabBarComponent
   ],
   imports: [
     BrowserModule,
